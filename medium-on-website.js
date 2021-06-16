d1c6555a0aa61c79b116e918f490a21ae073dc0f
Giacinto Carlucci <giacinto1996@live.it>
2021-06-16 15:17:10 +0200

    added author section + css improvements

--- medium-on-website.js
+++ medium-on-website.js
@@ -1,4 +1,5 @@
 const API_URL = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40giacintocarlucci&api_key=kgpvqeytkvzw21wkppfr58bfc5arfamrrfqep6ai";
+const AUTHOR_THUMBNAIL = "https://cdn-images-1.medium.com/fit/c/48/48/1*dtPbC22wRpXa0HDha3pGeA.jpeg"
 var feedData = {}
 
 const formatDate = (date) => {
@@ -36,12 +37,22 @@ const displayPosts = (data) => {
 		let HTMLPost = 
 		`
 			<div id="post-container">
+				<div id="post-header">
+					<div id="post-author-image">
+						<img src="${AUTHOR_THUMBNAIL}" alt="${post.author}"/>
+					</div>
+					<div id="post-author-info">
+						<div id="post-author">
+							${post.author}
+						</div>
+						<div id="post-date">
+							${formatDate(post.pubDate)}
+						</div>
+					</div>
+				</div>
 				<img id="post-image" src="${post.thumbnail}" alt="${post.title}"/>
 				<div id="post-title">
 					${post.title}
-				</div>
-				<div id="post-date">
-					${formatDate(post.pubDate)}
 				</div>
 				<div id="post-content">
 					${trimContent(post.description)}...
